35d0bcb5d76dbab39eb779051e00a0bbc58466b8
anibal-99 <anibal_99_@hotmail.com>
2023-05-18 08:56:25 -0300

    mas correcciones

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 dos2unix mvnw # convierte de formato, unix y windows, segun la maquina donde estes
 ./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005" &
-while true; do
-inotifywait -e modify,create,delete,move -r ./src/ && ./mvnw compile ;
-done > /dev/null
+while true; do inotifywait -e modify,create,delete,move -r ./src/ && ./mvnw compile ; done > /dev/null
