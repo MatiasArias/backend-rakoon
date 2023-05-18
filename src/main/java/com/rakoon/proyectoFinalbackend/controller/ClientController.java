989345d3f250042eb4ba66132fb4cccae5ee4da5
anibal-99 <anibal_99_@hotmail.com>
2023-05-18 08:52:02 -0300

    mas correcciones

--- src/main/java/com/rakoon/proyectoFinalbackend/controller/ClientController.java
+++ src/main/java/com/rakoon/proyectoFinalbackend/controller/ClientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/client/")
+@RequestMapping("/api/cliente/")
 @AllArgsConstructor
 public class ClientController {
     private ClientRepository clientRepository;
