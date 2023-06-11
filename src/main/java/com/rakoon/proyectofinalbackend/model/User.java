46079284a3715f32eb683a0c3a95ead945715471
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-06-10 21:28:02 -0300

    Update structure database name atribute
    Change camel case to snake case

--- src/main/java/com/rakoon/proyectofinalbackend/model/User.java
+++ src/main/java/com/rakoon/proyectofinalbackend/model/User.java
@@ -15,6 +15,7 @@ import java.time.LocalDate;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
     private String email;
     private String password;
