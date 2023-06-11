a42ca64edd8f89e03241e909f0c68fe391b80a0c
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-06-11 12:46:25 -0300

    Implements POST method to Consumer
    Create and implement method to create a new consumer

---
+++ src/main/java/com/rakoon/proyectofinalbackend/service/imp/ConsumerServiceImp.java
@@ -0,0 +1,40 @@
+package com.rakoon.proyectofinalbackend.service.imp;
+
+import com.rakoon.proyectofinalbackend.model.entity.Consumer;
+import com.rakoon.proyectofinalbackend.model.entity.User;
+import com.rakoon.proyectofinalbackend.model.view.ConsumerDto;
+import com.rakoon.proyectofinalbackend.repository.ConsumerRepository;
+import com.rakoon.proyectofinalbackend.service.ConsumerService;
+import com.rakoon.proyectofinalbackend.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerServiceImp implements ConsumerService {
+
+    ModelMapper modelMapper = new ModelMapper();
+
+    @Autowired
+    ConsumerRepository consumerRepository;
+    @Autowired
+    UserService userService;
+
+    @Override
+    public ConsumerDto save(ConsumerDto consumerDto) {
+        Consumer consumerPersisted = saveConsumer(consumerDto);
+        saveUser(consumerDto, consumerPersisted);
+        return modelMapper.map(consumerPersisted, ConsumerDto.class);
+    }
+
+    private Consumer saveConsumer(ConsumerDto consumerDto) {
+        Consumer consumer = modelMapper.map(consumerDto, Consumer.class);
+        return consumerRepository.save(consumer);
+    }
+
+    private void saveUser(ConsumerDto consumerDto, Consumer consumer) {
+        User user = modelMapper.map(consumerDto, User.class);
+        user.setConsumer(consumer);
+        userService.save(user);
+    }
+}
