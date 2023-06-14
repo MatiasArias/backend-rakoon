1ffc78255771da77de368557881a5793ed076671
Matias Arias <matiasarias384@gmail.com>
2023-06-13 21:34:30 -0300

    Add Sector Controller,Service and Repository

---
+++ src/main/java/com/rakoon/backend/repository/SectorRepository.java
@@ -0,0 +1,7 @@
+package com.rakoon.backend.repository;
+
+import com.rakoon.backend.model.entity.Sector;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectorRepository extends JpaRepository<Sector,Long> {
+}
