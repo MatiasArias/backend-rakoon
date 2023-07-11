c0452eb553d5a3c22a57c9507dd8b2a489a9ba38
Matias Arias <matiasarias384@gmail.com>
2023-07-11 11:02:36 -0300

    Upgrade TestEntityFactory

--- src/main/java/com/rakoon/backend/util/TestEntityFactory.java
+++ src/main/java/com/rakoon/backend/util/TestEntityFactory.java
@@ -1,16 +1,33 @@
 package com.rakoon.backend.util;
 
 import com.rakoon.backend.model.entity.Consumer;
+import com.rakoon.backend.model.entity.Establishment;
+import com.rakoon.backend.model.entity.Sector;
 import com.rakoon.backend.model.entity.User;
+import com.rakoon.backend.model.entity.WorkDay;
+import com.rakoon.backend.model.entity.WorkingDay;
 import com.rakoon.backend.model.view.ConsumerDto;
+import com.rakoon.backend.model.views.EstablishmentDto;
 import com.rakoon.backend.security.AuthCredentials;
+import com.rakoon.backend.security.UserDetailsImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestEntityFactory {
 
     static String password = new BCryptPasswordEncoder().encode("rakoon");
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = new UserDetailsImpl(getUserTest());
+        return new InMemoryUserDetailsManager(user);
+    }
     public static Consumer getConsumer(){
         return Consumer.builder()
                 .consumerId(1L)
@@ -25,6 +42,13 @@ public class TestEntityFactory {
         return User.builder()
                 .userId(1L)
                 .email("jalvarez@gmail.com")
+                .password(password)
+                .build();
+    }
+    public static User getUserTest(){
+        return User.builder()
+                .userId(912L)
+                .email("spring-test")
                 .password(password)
                 .build();
     }
@@ -47,5 +71,45 @@ public class TestEntityFactory {
                 .email("jalvarez@gmail.com")
                 .password("rakoon")
                 .build();
+    }
+    public static Establishment getEstablishment(){
+        return Establishment.builder()
+                .id(1L)
+                .name("Ala manchada")
+                .cuit("2030405060")
+                .description("Description")
+                .phone("154265376")
+                .sector(getSector())
+                .workDay(getWorkDayList())
+                .build();
+    }
+    public static EstablishmentDto getEstablishmentDto(){
+        return EstablishmentDto.builder()
+                .id(1L)
+                .name("Ala manchada")
+                .cuit("2030405060")
+                .description("Description")
+                .phone("154265376")
+                .build();
+    }
+    public static Sector getSector(){
+        return Sector.builder()
+                .id(1L)
+                .name("Restaurant")
+                .description("Description")
+                .build();
+    }
+    public static WorkDay getWorkDay(){
+        return WorkDay.builder()
+                .id(1L)
+                .workingDay(WorkingDay.MONDAY)
+                .timePickUpFrom("10:00")
+                .timePickUpTo("20:00")
+                .build();
+    }
+    public static List<WorkDay> getWorkDayList(){
+        List list = new ArrayList<>();
+        list.add(getWorkDay());
+        return list;
     }
 }
