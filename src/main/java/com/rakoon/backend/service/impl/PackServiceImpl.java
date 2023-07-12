500f89d8181323e24dd5de2fdaf19cb7dd0af586
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-07-12 01:29:26 -0300

    Add implementation to interface service PackService

---
+++ src/main/java/com/rakoon/backend/service/impl/PackServiceImpl.java
@@ -0,0 +1,22 @@
+package com.rakoon.backend.service.impl;
+
+import com.rakoon.backend.model.view.PackCardDto;
+import com.rakoon.backend.repository.PackRepository;
+import com.rakoon.backend.service.PackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class PackServiceImpl implements PackService {
+
+    @Autowired
+    PackRepository packRepository;
+
+    @Override
+    public List<PackCardDto> getPackCardInfo() {
+        return packRepository.findAllCardInformationPackages();
+    }
+}
