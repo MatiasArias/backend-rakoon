c48813364e9213604cba0fc8a15cfc3172a3dada
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-07-18 22:24:54 -0300

    Fix name of methods in controllers

--- src/main/java/com/rakoon/backend/controller/ConsumerController.java
+++ src/main/java/com/rakoon/backend/controller/ConsumerController.java
@@ -2,6 +2,7 @@ package com.rakoon.backend.controller;
 
 import com.rakoon.backend.model.view.ConsumerDto;
 import com.rakoon.backend.service.ConsumerService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/consumer")
+@Tag(name = "Consumer controller")
 public class ConsumerController {
 
     @Autowired
