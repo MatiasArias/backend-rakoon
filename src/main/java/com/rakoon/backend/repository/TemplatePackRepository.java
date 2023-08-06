d6ca946f1fa2fae20f4c97543aceed4eb5cd232f
Joaquin Alvarez <alvarezdjoaquin@gmail.com>
2023-08-05 21:07:41 -0300

    Create structure REST

---
+++ src/main/java/com/rakoon/backend/repository/TemplatePackRepository.java
@@ -0,0 +1,9 @@
+package com.rakoon.backend.repository;
+
+import com.rakoon.backend.model.entity.TemplatePack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemplatePackRepository extends JpaRepository<TemplatePack, Long> {
+
+}
+
---
+++ src/main/java/com/rakoon/backend/service/impl/TemplatePackServiceImpl.java
@@ -0,0 +1,33 @@
+package com.rakoon.backend.service.impl;
+
+import com.rakoon.backend.model.view.TemplatePackDto;
+import com.rakoon.backend.service.TemplatePackService;
+
+import java.util.List;
+
+public class TemplatePackServiceImpl implements TemplatePackService {
+    @Override
+    public List<TemplatePackDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public TemplatePackDto getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public TemplatePackDto update(Long id, TemplatePackDto template) {
+        return null;
+    }
+
+    @Override
+    public TemplatePackDto create(TemplatePackDto template) {
+        return null;
+    }
+}
